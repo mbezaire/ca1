fb314cebddb4264a22e88018c43f6b657fe508cf
András Ecker <andras.ecker@epfl.ch>
2017-09-12 14:20:32 +0200

    download scripts added

--- NSG/list_results.sh
+++ NSG/list_results.sh
@@ -4,7 +4,6 @@ source ./read_env_variables
 
 echo "Accessing: $URL/job/$USERNAME/$1 for user $USERNAME"
 
-curl --user $USERNAME:$PASSWORD \
+curl -i --user $USERNAME:$PASSWORD \
      -H cipres-appkey:$DIRECT_APPID \
-     -O -J \
-     $URL/job/$USERNAME/$1/output/$2
+      $URL/job/$USERNAME/$1/output
