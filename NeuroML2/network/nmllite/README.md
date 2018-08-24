cd741b0b3fe8920572f5dd52fcfe36803b7514bd
Padraig Gleeson <p.gleeson@gmail.com>
2018-08-24 16:58:38 +0100

    Cell info

---
+++ NeuroML2/network/nmllite/README.md
@@ -0,0 +1,110 @@
+### CA1 cell summary 
+<table>
+<tr>
+  <td><b>sca</b></td>
+  <td><a href="mean_spike_frequency_sca.png">
+    <img alt="?" src="mean_spike_frequency_sca.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_sca.png">
+    <img alt="?" src="firing_rates_sca.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_sca.png">
+    <img alt="?" src="heatmap_sca.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>olm</b></td>
+  <td><a href="mean_spike_frequency_olm.png">
+    <img alt="?" src="mean_spike_frequency_olm.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_olm.png">
+    <img alt="?" src="firing_rates_olm.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_olm.png">
+    <img alt="?" src="heatmap_olm.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>pvbasket</b></td>
+  <td><a href="mean_spike_frequency_pvbasket.png">
+    <img alt="?" src="mean_spike_frequency_pvbasket.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_pvbasket.png">
+    <img alt="?" src="firing_rates_pvbasket.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_pvbasket.png">
+    <img alt="?" src="heatmap_pvbasket.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>ivy</b></td>
+  <td><a href="mean_spike_frequency_ivy.png">
+    <img alt="?" src="mean_spike_frequency_ivy.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_ivy.png">
+    <img alt="?" src="firing_rates_ivy.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_ivy.png">
+    <img alt="?" src="heatmap_ivy.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>ngf</b></td>
+  <td><a href="mean_spike_frequency_ngf.png">
+    <img alt="?" src="mean_spike_frequency_ngf.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_ngf.png">
+    <img alt="?" src="firing_rates_ngf.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_ngf.png">
+    <img alt="?" src="heatmap_ngf.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>bistratified</b></td>
+  <td><a href="mean_spike_frequency_bistratified.png">
+    <img alt="?" src="mean_spike_frequency_bistratified.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_bistratified.png">
+    <img alt="?" src="firing_rates_bistratified.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_bistratified.png">
+    <img alt="?" src="heatmap_bistratified.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>cck</b></td>
+  <td><a href="mean_spike_frequency_cck.png">
+    <img alt="?" src="mean_spike_frequency_cck.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_cck.png">
+    <img alt="?" src="firing_rates_cck.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_cck.png">
+    <img alt="?" src="heatmap_cck.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>axoaxonic</b></td>
+  <td><a href="mean_spike_frequency_axoaxonic.png">
+    <img alt="?" src="mean_spike_frequency_axoaxonic.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_axoaxonic.png">
+    <img alt="?" src="firing_rates_axoaxonic.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_axoaxonic.png">
+    <img alt="?" src="heatmap_axoaxonic.png" height="180"/></a>
+  </td>
+<tr>
+<tr>
+  <td><b>poolosyn</b></td>
+  <td><a href="mean_spike_frequency_poolosyn.png">
+    <img alt="?" src="mean_spike_frequency_poolosyn.png" height="180"/></a>
+  </td>
+  <td><a href="firing_rates_poolosyn.png">
+    <img alt="?" src="firing_rates_poolosyn.png" height="180"/></a>
+  </td>
+  <td><a href="heatmap_poolosyn.png">
+    <img alt="?" src="heatmap_poolosyn.png" height="180"/></a>
+  </td>
+<tr>
--- NeuroML2/network/nmllite/Sweep.py
+++ NeuroML2/network/nmllite/Sweep.py
@@ -15,7 +15,11 @@ if __name__ == '__main__':
 
     if '-all' in sys.argv:
         
-        fixed = {'dt':0.001, duration:700}
+        print('Generating all plots')
+        save_fig_dir = './'
+        html = '<table>\n'
+        
+        fixed = {'dt':0.001, 'duration':700}
 
 
         vary = {'stim_amp':['%spA'%(i) for i in xrange(-50,400,50)]}
@@ -25,29 +29,56 @@ if __name__ == '__main__':
         for type in colors:
             if type!='ec' and type !='ca3':
 
-                nmllr = NeuroMLliteRunner('Sim_IClamp_%s.json'%type,
-                                          simulator='jNeuroML_NEURON')
+                run = True
+                #run = False
+                
+                if run:
+                
+                    nmllr = NeuroMLliteRunner('Sim_IClamp_%s.json'%type,
+                                              simulator='jNeuroML_NEURON')
+                    ps = ParameterSweep(nmllr, 
+                                        vary, 
+                                        fixed,
+                                        num_parallel_runs=16,
+                                      save_plot_all_to='firing_rates_%s.png'%type,
+                                      heatmap_all=True,
+                                      save_heatmap_to='heatmap_%s.png'%type,
+                                        plot_all=True, 
+                                        show_plot_already=False)
 
-                ps = ParameterSweep(nmllr, 
-                                    vary, 
-                                    fixed,
-                                    plot_all=True, 
-                                    heatmap_all=True,
-                                    show_plot_already=False)
+                    report = ps.run()
 
-                report = ps.run()
-
-                #ps.plotLines('stim_amp','average_last_1percent',save_figure_to='average_last_1percent_%s.png'%type)
-                ps.plotLines('stim_amp','mean_spike_frequency',save_figure_to='mean_spike_frequency_%s.png'%type)
+                    #ps.plotLines('stim_amp','average_last_1percent',save_figure_to='average_last_1percent_%s.png'%type)
+                    ps.plotLines('stim_amp','mean_spike_frequency',save_figure_to='mean_spike_frequency_%s.png'%type)
+                
+                
+                html+='<tr>\n'
+                html+='  <td><b>'+type+'</b></td>\n'
+                html+='  <td><a href="mean_spike_frequency_%s.png'%type+'">\n'
+                html+='    <img alt="?" src="mean_spike_frequency_%s.png'%type+'" height="180"/></a>\n'
+                html+='  </td>\n'
+                html+='  <td><a href="firing_rates_%s.png'%type+'">\n'
+                html+='    <img alt="?" src="firing_rates_%s.png'%type+'" height="180"/></a>\n'
+                html+='  </td>\n'
+                html+='  <td><a href="heatmap_%s.png'%type+'">\n'
+                html+='    <img alt="?" src="heatmap_%s.png'%type+'" height="180"/></a>\n'
+                html+='  </td>\n'
+                html+='<tr>\n'
 
         import matplotlib.pyplot as plt
         if not '-nogui' in sys.argv:
             print("Showing plots")
             plt.show()
+            
+            
+        with open(save_fig_dir+'info.html','w') as f:
+            f.write('<html><body>\n%s\n</body></html>'%html)
+        with open(save_fig_dir+'README.md','w') as f2:
+            f2.write('### CA1 cell summary \n%s'%(html.replace('.html','.md')))
         
     else:
         
-        fixed = {'dt':0.001, duration:700}
+        fixed = {'dt':0.01, 'duration':700}
 
         quick = False
         #quick=True
@@ -78,7 +109,9 @@ if __name__ == '__main__':
         ps = ParameterSweep(nmllr, vary, fixed,
                             num_parallel_runs=16,
                                   plot_all=True, 
+                                  save_plot_all_to='firing_rates_%s.png'%type,
                                   heatmap_all=True,
+                                  save_heatmap_to='heatmap_%s.png'%type,
                                   show_plot_already=False)
 
         report = ps.run()
