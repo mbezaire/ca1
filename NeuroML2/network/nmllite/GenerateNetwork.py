e3945cc0588145965cdbf68120405738cdbf2f77
Padraig Gleeson <p.gleeson@gmail.com>
2018-12-04 14:55:25 +0000

    Simple connections

--- NeuroML2/network/nmllite/GenerateNetwork.py
+++ NeuroML2/network/nmllite/GenerateNetwork.py
@@ -1,6 +1,5 @@
 from neuromllite import *
 from neuromllite.NetworkGenerator import *
-from neuromllite.utils import create_new_model
 import sys
 
 sys.path.append("..")
@@ -9,7 +8,7 @@ from GenerateHippocampalNet_oc import helper_getcolor
 
 colors = helper_getcolor(None)
 
-def generate(cell_numbers, duration, dt=0.025, simulation_seed=1234, config='Net'):
+def generate(cell_numbers, input_num_freqs, duration, dt=0.025, simulation_seed=1234, config='Net'):
     
     reference = "%s"%(config)
     
@@ -17,8 +16,11 @@ def generate(cell_numbers, duration, dt=0.025, simulation_seed=1234, config='Net
     net.notes = "A network model: %s"%reference
     net.temperature = 34 # degC
     
-    r1 = RectangularRegion(id='ca1', x=0,y=0,z=0,width=1000,height=100,depth=1000)
-    net.regions.append(r1)
+    ca1 = RectangularRegion(id='ca1', x=0,y=0,z=0,width=1000,height=100,depth=1000)
+    net.regions.append(ca1)
+    
+    ext = RectangularRegion(id='ext', x=1500,y=0,z=0,width=1000,height=100,depth=1000)
+    net.regions.append(ext)
     
     net.parameters = {}
     net.parameters['stim_amp'] = '350pA'
@@ -31,32 +33,55 @@ def generate(cell_numbers, duration, dt=0.025, simulation_seed=1234, config='Net
         
         net.cells.append(cell_nmll)
         
-        pop = Population(id='pop_%s'%cell_id, 
+        pop = Population(id='pop_%s'%cell, 
                             size=cell_numbers[cell], 
                             component=cell_id, 
                             properties={'color':colors[cell]})
 
-        pop.random_layout = RandomLayout(region=r1.id)
+        pop.random_layout = RandomLayout(region=ca1.id)
 
         net.populations.append(pop)
  
-        ################################################################################
-        ###   Add some inputs
+    net.id = reference 
 
+    ################################################################################
+    ###   Add some inputs
+    for input in input_num_freqs:
 
-        input_source = InputSource(id='iclamp_0', 
-                                   neuroml2_input='PulseGenerator', 
-                                   parameters={'amplitude':'stim_amp', 'delay':'100ms', 'duration':'500ms'})
+        num, freq = input_num_freqs[input]
+        ssp = Cell(id='%scell'%input, pynn_cell='SpikeSourcePoisson')
 
+        ssp.parameters = { 'rate':       '%s'%freq,
+                         'start':      0,
+                         'duration':   1e9}
+        net.cells.append(ssp)
 
-        net.input_sources.append(input_source)
+        pop = Population(id='pop_%s'%input, 
+                            size=num, 
+                            component=ssp.id, 
+                            properties={'color':colors[input]})
 
+        pop.random_layout = RandomLayout(region=ext.id)
 
-        net.inputs.append(Input(id='Stim_%s'%input_source.id,
-                                input_source=input_source.id,
-                                population=pop.id,
-                                percentage=100))
-      
+        net.populations.append(pop)
+            
+            
+                                
+
+    for cell in cell_numbers.keys():
+        for input in input_num_freqs:
+            
+            syn_id = 'syn_%s_to_%s'%(input, cell)
+            net.synapses.append(Synapse(id=syn_id, neuroml2_source_file='../../synapses/exp2Synapses.synapse.nml'))
+            
+            net.projections.append(Projection(id='proj_%s_%s'%(input, cell),
+                                      presynaptic='pop_%s'%input, 
+                                      postsynaptic='pop_%s'%cell,
+                                      synapse=syn_id,
+                                      delay=0,
+                                      weight=1,
+                                      random_connectivity=RandomConnectivity(probability=.5)))
+            
         
                                                
     network_filename = '%s.json'%reference         
@@ -91,7 +116,8 @@ if __name__ == "__main__":
         
     else:
         
-        sim, net = generate({'olm':5}, 1000)
+        #sim, net = generate({'olm':5}, 1000)
+        sim, net = generate({'ngf':5}, {'ec':(5,50)}, 1000)
         
         check_to_generate_or_run(sys.argv, sim)
     
